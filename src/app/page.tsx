c0a8e54f262f05ac55bb1777fc3a2406bf18bb1b
amauridevC <amaurislbertosena@gmail.com>
2025-07-02 09:44:26 -0300

    movendo o componente de header para ficar somente na tela de inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import CardAnimalPerdidos from "@/components/cards/cardAnimalPerdidos";
 import CardInicial from "@/components/cards/cardInicial";
 import { Container } from "@/components/container";
+import Header from "@/components/header";
 import PainelPerdidos from "@/components/painelPerdidos";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
+      <header>
+        <Header/>
+      </header>
       <main>
         <section>
           <div>
